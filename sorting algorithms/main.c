49967e056c38bb8ba73e743dcb5f0d908b3c7bac
clayton-halim <ClaytonHalim3@gmail.com>
2017-02-26 15:16:39 -0500

    First commit

---
+++ sorting algorithms/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "sort.h"
+
+void print_array(int arr[], int len);
+
+int main(void) {
+	int lst[] = {6, 5, 4, 3, 3, 2, 1};
+	int len = sizeof(lst) / sizeof(lst[0]);
+	printf("Original array: ");
+	print_array(lst, len);
+	printf("Length of array: %d\n", len);
+
+	insertion_sort(lst, len);
+	printf("Sorted array: ");
+	print_array(lst, len);
+}
+
+void print_array(int arr[], int len) {
+	for (int i = 0; i < len - 1; ++i) {
+		printf("%d ", arr[i]);
+	}
+
+	printf("%d\n", arr[len - 1]);
+}
